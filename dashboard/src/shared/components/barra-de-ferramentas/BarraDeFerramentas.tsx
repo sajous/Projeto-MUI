eea711af22e96fcb1d6f77e572d6fcea69754b61
Patrick Sajous <patricksajous17@gmail.com>
2022-09-16 09:52:28 -0300

    feat:Concluido Barra de Pesquisa

--- dashboard/src/shared/components/barra-de-ferramentas/BarraDeFerramentas.tsx
+++ dashboard/src/shared/components/barra-de-ferramentas/BarraDeFerramentas.tsx
@@ -19,7 +19,7 @@ export const BarraDeFerramentas: React.FC<IBarraDeFerramentasProps> =  ({
 }) =>{
   const theme = useTheme();
   return(
-    <Box heigth={theme.spacing(5)} 
+    <Box height={theme.spacing(5)} 
       marginX={1} 
       padding={1} 
       paddingX={2} 
@@ -36,16 +36,16 @@ export const BarraDeFerramentas: React.FC<IBarraDeFerramentasProps> =  ({
         />
       )}
       <Box flex={1} display="flex" justifyContent="end">
-      {mostrarBotaoNovo &&(
+        {mostrarBotaoNovo &&(
           <Button
-          variant='contained'
-          color ='primary'
-          disableElevation
-          onclick={aoClicarEmNovo}
-          endIcon={<Icon>add</Icon>}
-        >{textoBotaoNovo}</Button>
+            color="primary"
+            disableElevation
+            variant="contained"
+            onClick={aoClicarEmNovo}
+            endIcon={<Icon>add</Icon>}
+          >{textoBotaoNovo}</Button>
+        )};
       </Box>
-      )};
     </Box>
   );
 };
